5d7042c19f5cd4580a1e3573fd22f24406457046
Giulio Fantuzzi <74678041+giuliofantuzzi@users.noreply.github.com>
2022-11-10 17:18:45 +0100

    Update lab_04.R

--- laboratory/lab_04.R
+++ laboratory/lab_04.R
@@ -10,8 +10,7 @@ e2 <- c(-0.1,0.7,0.5,-0.3,0.4,-0.1,-0.2,0,0.1,-0.2,0.8,0.6,-0.9,-0.3,-0.1,0.9,-0
 
 plot(e1, type="l")
 abline(h=0, lty=2)
-lines(ma(e1,5), col="red")
-#ma è funzione perfare medie mobili...questa è media mobile di ordine 5
+lines(ma(e1,5), col="red")#ma è funzione perfare medie mobili...questa è media mobile di ordine 5
 plot(ma(e1, 5), col="red", type="l")
 
 ma5.e1 <- ma(e1, 5)[-c(1,2,length(e1)-1, length(e1))]
@@ -37,17 +36,17 @@ y
 ma(y, 5)
 
 
-## trend lineare pi? disturbo
+## costruisco una serie del tipo trend lineare+disturbo white noise
 y <- 1+0.7*ttrend[-1] + rnorm(length(y))
-lines(y, col="blue")
-lines(ma(y, 5), col="red")
+lines(y, col="blue") #la serie
+lines(ma(y, 5), col="red") #media mobile della serie
 
 
-## serie periodica
+## Vediamo ora una serie periodica
 x <- rep(c(3.5, 5, 4, 6), 5)
 
 plot(x, type="l")
-lines(ma(x, 4), col="blue")
+lines(ma(x, 4), col="blue") #media della serie
 
 #------------------------------------------
 ## Tab. 3.5
@@ -71,7 +70,7 @@ dati$S.star <- tapply(dati$IS, dati$trim,
 ## normalizzo stag grezza a 1
 coefst <- prod(dati$S.star)^(1/20)
 dati$S <- dati$S.star/coefst
-## calcolo serie destagionalizzata
+## calcolo serie destagionalizzata (faccio il rapporto poichè la serie era moltiplicativa)
 dati$y.d <- dati$y/dati$S
 lines(dati$y.d, col="blue")#sta linea blu è la serie destagionalizzata (ma comprensiva di trend giusto?)
 ###################################
@@ -88,7 +87,7 @@ abline(h=0, lty=3)
 
 ## scriviamo una funzione "media mobile (semplice)"
 
-myma <- function(x, order=5) {
+myma <- function(x, order=5) {#ordine 5 di default
     semismm <- ((order-1)/2)
     inizio <- 1 + semismm
     fine <- length(x) - semismm
@@ -99,27 +98,21 @@ myma <- function(x, order=5) {
     return(mmx)
 }
 
-lines(myma(y), col="red")
-lines(myma(y, 9), col="blue")
+lines(myma(y), col="red") #ma di ordine 5
+lines(myma(y, 9), col="blue")#ma di ordine 9
 
 var(y)
 var(myma(y, 5), na.rm=T)
-var(myma(y,9), na.rm=T)
+var(myma(y,9), na.rm=T) #usando ma di ordine 9 la varianza è minore rispetto a ordine 5
 
-lines(myma(y), col="red")
-lines(myma(y, 9), col="blue")
-var(y)
-var(myma(y, 5)) # bisogna rimuovere gli NA
-var(myma(y, 5), na.rm=T)
-var(myma(y, 9), na.rm=T)
 
-## rapporto di riduzione della varianza residua:
-## 5
-sum(rep(1/5, 5)^2)
-
-## in generale (per medie semplici!)
+## funzione che calcola il rapporto di riduzione della varianza residua:
+## in generale (per medie semplici!) vale
 rrv <- function(order) sum(rep(1/order, order)^2)
 
-## 9
+## Confronto tra ordine 5 e 9
+rrv(5)
 rrv(9)
-
+#NB: entrambi<1: BENE
+#    rrv(9)< rrv(5)--->usando la ma di ordine 9 la varianza si riduce di più rispetto ad usare ordine 5
+#    e avevamo già notato poco fa che var(myma(y, 5), na.rm=T) > var(myma(y, 9), na.rm=T)
